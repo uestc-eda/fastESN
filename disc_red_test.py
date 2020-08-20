476ba68a7b7271e62ec8cbdb7ce4a586de9d82d3
Hai <wanghai@uestc.edu.cn>
2020-08-21 00:09:11 +0800

    add MIMO support and change to control module

--- disc_red_test.py
+++ disc_red_test.py
@@ -1,19 +1,28 @@
 import numpy as np
-from scipy import signal
+import control
 import matplotlib.pyplot as plt
 
-a = np.array([[1, 2], [3, 4]])
-b = np.array([[5], [6]])
-c = np.array([[7, 8]])
-d = np.array([[9]])
+n_org = 10
+n_in = 2
+n_out = 2
 
-sys = signal.dlti(a, b, c, d)
+a = np.random.rand(n_org, n_org)
+b = np.random.rand(n_org, n_in)
+c = np.random.rand(n_out, n_org)
+d = np.random.rand(n_out, n_in)
+
+sys = control.ss(a, b, c, d, 0.1)
 print(sys)
 
-w, mag, phase = sys.bode()
+in_plot = 0 # the input index (starting from 0) of the frequence response to be ploted
+out_plot = 0 # the output index (starting from 0) of the frequence response to be ploted
+sys_plot = control.ss(a, b[:, [in_plot]], c[[out_plot], :], d[[out_plot], [in_plot]], 0.1) # the SISO system (extracted from the MIMO system) to be ploted
+print(sys_plot)
 
-plt.figure()
-plt.semilogx(w, mag)
-plt.figure()
-plt.semilogx(w, phase)
+mag, phase, w = control.bode(sys_plot)
+# (mag, phase, freq) = sys.freqresp(freq)
+# plt.figure()
+# plt.semilogx(w, mag)
+# plt.figure()
+# plt.semilogx(w, phase)
 plt.show()
