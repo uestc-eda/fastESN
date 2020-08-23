9dde315504486c23491589428c636b0ee378e1b7
Hai Wang <wanghai@uestc.edu.cn>
2020-08-24 02:24:29 +0800

    update

--- disc_red_test.py
+++ disc_red_test.py
@@ -1,10 +1,13 @@
 import numpy as np
 import control
 import matplotlib.pyplot as plt
+from mor_disc import mor_disc
 
 n_org = 10
-n_in = 2
-n_out = 2
+n_in = 1
+n_out = 1
+
+omega_samples = np.array([0.1, 1.4, 2.4])
 
 a = np.random.rand(n_org, n_org)
 b = np.random.rand(n_org, n_in)
@@ -14,14 +17,20 @@ d = np.random.rand(n_out, n_in)
 sys = control.ss(a, b, c, d, 1)
 print(sys)
 
+sys_r = mor_disc(sys, omega_samples)
+print(sys_r)
 
 
 in_plot = 0 # the input index (starting from 0) of the frequence response to be ploted
 out_plot = 0 # the output index (starting from 0) of the frequence response to be ploted
-sys_plot = control.ss(a, b[:, [in_plot]], c[[out_plot], :], d[[out_plot], [in_plot]], 1) # the SISO system (extracted from the MIMO system) to be ploted
+sys_plot = control.ss(sys.A, sys.B[:, [in_plot]], sys.C[[out_plot], :], sys.D[[out_plot], [in_plot]], 1) # the SISO system (extracted from the MIMO system) to be ploted
 print(sys_plot)
 
+sys_r_plot = control.ss(sys_r.A, sys_r.B[:, [in_plot]], sys_r.C[[out_plot], :], sys_r.D[[out_plot], [in_plot]], 1)
+
 mag, phase, w = control.bode(sys_plot)
+
+mag_r, phase_r, w_r = control.bode(sys_r_plot)
 # (mag, phase, freq) = sys.freqresp(freq)
 # plt.figure()
 # plt.semilogx(w, mag)
--- mor_disc.py
+++ mor_disc.py
@@ -1,13 +1,35 @@
-def mor_disc(sys, f_samples)
+import numpy as np
+import control
+
+def mor_disc(sys, omega_samples):
     n = sys.A.shape[0] # order of the system
     n_in = sys.B.shape[1] # input number of the system
     n_out = sys.C.shape[0] # output number of the system
-    n_samples = f_samples.size # number of frequency sample points
-    M = np.zeros(shape=(n, 2*n_samples*n_in))
+    n_samples = omega_samples.size # number of frequency sample points
+    
+    M = np.zeros(shape=(n, 2*n_samples*n_in), dtype=complex)
     index = 0
-    for f_sample in f_samples:
-        m = sys.C*np.linalg.solve(exp(j*f_sample)*np.identity(n)-sys.A, sys.B) # solve system for f_sample: m = C (zI - A)^{-1} B
+    for omega_sample in omega_samples:
+        m = np.linalg.solve(np.exp(1j*omega_sample)*np.identity(n)-sys.A, sys.B) # solve system for omega_sample: m = (zI - A)^{-1} B
         M[:,2*n_in*index:2*n_in*index+n_in] = m
         M[:,2*n_in*index+n_in:2*n_in*index+2*n_in] = np.conj(m)
-    U, S, V = np.linalg.svd(M, full_matrices=False)
+        index = index + 1
+
+    print(M)
     
+    U, S, V = np.linalg.svd(M, full_matrices=False)
+
+    # convert U to real matrix by angle rotation
+    u_angle = np.angle(U[0,:])
+    for i in range(U.shape[1]):
+        U[:,i] = U[:,i]*np.exp(-1j*u_angle[i])
+    U = U.real
+
+    Ar = U.T@sys.A@U
+    Br = U.T@sys.B
+    Cr = sys.C@U
+    Dr = sys.D
+
+    sys_r = control.ss(Ar, Br, Cr, Dr, 1)
+
+    return sys_r
