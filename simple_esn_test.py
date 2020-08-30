20e4e4645247cd8caf488b4892050f02513f5cdc
Hai <wanghai@uestc.edu.cn>
2020-08-31 02:37:19 +0800

    add state space model

--- simple_esn_test.py
+++ simple_esn_test.py
@@ -19,9 +19,9 @@ except:
 random_seed = np.frombuffer(os.urandom(4), dtype=np.uint32)[0]
 print("seed: ", random_seed)
 
-batches = 20
+# batches = 100
 stime = 50
-epochs = 400
+epochs = 1000
 num_units = 50
 num_inputs = 1
 num_outputs = 1
@@ -29,7 +29,8 @@ num_outputs = 1
 activation = lambda x: math_ops.tanh(x)
 
 t = np.linspace(0, 1, stime).reshape(1,-1, 1).astype("float32")
-inputs = np.exp(-t*200)
+# inputs = np.exp(-t*200)
+inputs = np.exp(np.sin(t*6))
 targets = np.cos(t*6*np.pi)
 
 plt.figure()
@@ -38,21 +39,21 @@ t, = plt.plot(targets[0], color="black")
 plt.xlabel("Timesteps")
 plt.legend([i, t], ['input', "target"])
 
-# Init the ESN cell
-cell = EchoStateRNNCell(units=num_units, 
-                        activation=activation, 
-                        decay=0.1, 
-                        epsilon=1e-20,
-                        alpha=0.5,
-                        optimize=True,
-                        optimize_vars=["rho", "decay", "alpha", "sw"],
-                        seed=random_seed)
+# # Init the ESN cell
+# cell = EchoStateRNNCell(units=num_units, 
+#                         activation=activation, 
+#                         decay=0.1, 
+#                         epsilon=1e-20,
+#                         alpha=0.5,
+#                         optimize=True,
+#                         optimize_vars=["rho", "decay", "alpha", "sw"],
+#                         seed=random_seed)
 
-# Build the recurrent layer containing the ESN cell
-recurrent_layer = keras.layers.RNN(cell, input_shape=(stime, num_inputs), 
-                                   return_sequences=True, name="nn")
+# # Build the recurrent layer containing the ESN cell
+# recurrent_layer = keras.layers.RNN(cell, input_shape=(stime, num_inputs), 
+#                                    return_sequences=True, name="nn")
 
-# recurrent_layer = tfa.layers.ESN(units=num_units, connectivity=1, input_shape=(stime, num_inputs), return_sequences=True, name="nn")
+recurrent_layer = tfa.layers.ESN(units=num_units, leaky=1, activation='tanh', connectivity=0.5, input_shape=(stime, num_inputs), return_sequences=True, use_bias=False, name="nn")
 
 # Build the readout layer
 output = keras.layers.Dense(num_outputs, name="readouts")
@@ -96,14 +97,41 @@ plt.ylim([-1.1,1.5])
 plt.yticks([-1, -0.5, 0, 0.5, 1])
 plt.legend([r[0],o], ["network activity", "readout"])
 
+# plt.show()
+# print(model.layers[0].weights)
+# print(model.layers[1].weights)
+
+# extract the ESN model in state space form
+W = tf.transpose(model.layers[0].weights[0])
+W_in = tf.transpose(model.layers[0].weights[1])
+print("W_in=", W_in)
+print("W=", W)
+
+W_out = tf.transpose(model.layers[1].weights[0])
+out_bias = tf.transpose(model.layers[1].weights[1])
+print("W_out=", W_out)
+print("out_bias=", out_bias)
+
+# simulate the state space ESN model
+x_pre = np.zeros((num_units,1)) # initiate state as zeros if esn model use default zero initial state
+y_out = np.zeros((num_outputs, inputs.shape[1])) # output matrix, composed of output vectors over time
+print("shape_inputs: ", inputs.shape)
+for i in range(inputs[0].shape[0]):    
+    x_cur = np.tanh(W@x_pre + W_in@tf.reshape(inputs[0,i,:],[1,1]))
+    y_out[:,i] = W_out @ x_cur + out_bias
+    x_pre = x_cur
+print("y_out=", y_out)
+
 plt.figure()
 outputs = model(inputs) 
 i, = plt.plot(inputs[0], color="blue")
 t, = plt.plot(targets[0], color="#44ff44", lw=10)
 o, = plt.plot(outputs[0], color="#ff4444", lw=4)
+m, = plt.plot(y_out[0], color="black", linestyle='dashed')
 plt.xlabel("Timesteps")
-plt.legend([i, t, o], ['input', "target", "readout"])
+plt.legend([i, t, o, m], ['input', "target", "readout", "ss model"])
 
 plt.show()
 
-print(model.layers[0].weights)
+
+
